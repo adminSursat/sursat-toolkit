ac7a9473793ef5b5107b051ccf1c477b21adfa4c
adminSursat <sursat.adm3@gmail.com>
2022-11-21 19:52:37 -0300

    update celus + filtro aperturas

--- scripts/templadosScript.js
+++ scripts/templadosScript.js
@@ -5,15 +5,44 @@ function filtrar() {
   filter = input.value.toUpperCase();
   table = document.getElementById("templadosTable");
   tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[1];
-    if (td) {
+  for(i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[2];
+    if(td) {
       txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      if(txtValue.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
-      } else {
+      } 
+      else {
         tr[i].style.display = "none";
       }
     }       
   }
+}
+
+//filtra los vidrios que son abiertos cerrados o muestra todos
+function tipoApertura() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("abiertoCerrado");
+  filter = input.value.toUpperCase();
+  console.log(filter);
+  table = document.getElementById("templadosTable");
+  tr = table.getElementsByTagName("tr");
+  if(filter == "TODOS") {
+    for(i = 0; i < tr.length; i++) {
+      tr[i].style.display = "";
+    }
+  }   
+  else {
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[1];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }  
 }
